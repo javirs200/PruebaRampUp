5de64f881fcad6eec6439e11b6dd0d03b035d969
rsmax <fordrsmax@gmail.com>
2023-09-28 11:49:10 +0200

    format

--- script.js
+++ script.js
@@ -8,51 +8,51 @@ Entregad vuestra solución por classroom. Si os da tiempo, pensad varias maneras
 
 
 //ejercicio 1 en caso de que haya que diferenciar mayusculas de minusculas (estrictamente iguales)
-function contarOcurrencias(letra,frase) {
+function contarOcurrencias(letra, frase) {
 
     let contador = 0;
-  
+
     for (let i = 0; i < frase.length; i++) {
-     
-      if (frase[i] === letra) {
-        contador++
-      }
+
+        if (frase[i] === letra) {
+            contador++
+        }
     }
-  
+
     return contador
-  }
+}
 
 
 //ejercicio 1 en caso de que solo interese la letra sin ser caseSensitive
-function contarOcurrencias(letra,frase) {
+function contarOcurrencias(letra, frase) {
 
     let contador = 0;
-  
+
     let minusfrase = frase.toLowerCase()
     let minusletra = letra.toLowerCase()
-  
+
     for (let i = 0; i < minusfrase.length; i++) {
-     
-      if (minusfrase[i] === minusletra) {
-        contador++
-      }
+
+        if (minusfrase[i] === minusletra) {
+            contador++
+        }
     }
-  
+
     return contador
-  }
-  
-  
-  //ejercicio 2 
-  function dameImpares() {
-  
+}
+
+
+//ejercicio 2 
+function dameImpares() {
+
     const numeroUsuario = parseInt(prompt("Por favor, ingresa un número:"));
 
     if (!isNaN(numeroUsuario)) {
-    
+
         for (let i = numeroUsuario; i < numeroUsuario + 50; i++) {
-            if(i % 2 !== 0){
+            if (i % 2 !== 0) {
                 console.log(i);
             }
-        }  
+        }
     }
 }
