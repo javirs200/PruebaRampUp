aa2b3a6a85276112d24230e65aaba94611ecea5c
rsmax <fordrsmax@gmail.com>
2023-09-28 11:47:04 +0200

    enunciado

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Ejercicio 1:
+Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
+
+Ejercicio 2:
+Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
+
+Entregad vuestra solución por classroom. Si os da tiempo, pensad varias maneras de resolverlo y enviad vuestras soluciones. Podéis enviar una primera solución y luego añadir otras en comentarios.*/
+
+
